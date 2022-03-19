658707026e29a716bd3e51b0b1f9389d13c0035c
MarinaSeiken <marina.seiken@outlook.com>
2022-03-19 17:58:47 -0600

    Flask app to run model in a web app

---
+++ src/flask_app.py
@@ -0,0 +1,26 @@
+import pickle
+import pandas as pd
+from flask import Flask, request, jsonify
+
+from src.model_training import LeadScoringModel
+
+
+app = Flask(__name__)
+model = pickle.load(open('../model/lead_scoring_model_2022-03-19.pkl', 'rb'))
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    data = request.get_json(force=True)
+    data = pd.DataFrame.from_records([data])
+    app.logger.info(f'{data.shape}')
+
+    prediction = model.predict_proba(data)[:,1]
+    app.logger.info(prediction)
+
+    return jsonify(prediction[0])
+
+
+if __name__ == "__main__":
+    app.run()
